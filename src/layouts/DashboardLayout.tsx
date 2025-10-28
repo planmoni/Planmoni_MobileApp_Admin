ff3f446286d8040e96a05bd7f3acc48d42529ca2
mosodi007 <martins.osodi@gmail.com>
2025-10-28 18:51:53 +0100

    Updated DashboardLayout.tsx

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -30,7 +30,7 @@ export default function DashboardLayout() {
           <img
             src="/assets/images/planmoni_logo_main.png"
             alt="Planmoni Office"
-            className="h-12 w-auto"
+            className="h-10 w-auto"
             onError={(e) => {
               e.currentTarget.style.display = 'none';
               const nextElement = e.currentTarget.nextElementSibling as HTMLElement;
