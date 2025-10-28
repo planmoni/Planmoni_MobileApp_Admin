11c3ccdac62db79b3491441021fd3777fbae07e8
mosodi007 <martins.osodi@gmail.com>
2025-10-28 18:58:54 +0100

    Updated Dashboard.tsx

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -91,7 +91,9 @@ export default function Dashboard() {
 
   if (isLoading) {
     return (
-      <LoadingScreen />
+      <div className="flex justify-center items-center h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-accent"></div>
+      </div>
     );
   }
 
