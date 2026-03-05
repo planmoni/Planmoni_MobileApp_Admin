57bc781b0a0fc08fe7566819e20d699b9f0bf7c1
mosodi007 <martins.osodi@gmail.com>
2026-03-05 14:22:05 +0100

    Updated UserDetails.tsx

--- src/pages/dashboard/UserDetails.tsx
+++ src/pages/dashboard/UserDetails.tsx
@@ -783,7 +783,7 @@ export default function UserDetails() {
 
       <div>
         <h2 className="text-xl font-bold text-gray-900 mb-4">User Activity</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
 
           <div className="bg-white rounded-2xl p-6 shadow-soft border border-gray-100">
             <div className="mb-6">
