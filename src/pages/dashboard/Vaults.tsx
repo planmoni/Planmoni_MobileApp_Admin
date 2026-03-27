2715a79baa88d85788de741d679d346c0ee9a9c2
mosodi007 <martins.osodi@gmail.com>
2026-03-27 15:07:23 +0100

    Updated Vaults.tsx

--- src/pages/dashboard/Vaults.tsx
+++ src/pages/dashboard/Vaults.tsx
@@ -120,7 +120,7 @@ export default function Vaults() {
   };
 
   const balanceBarData = {
-    labels: ['Total Funded', 'Current Balance', 'Remaining to fund'],
+    labels: ['Total in Vault', 'Current Balance', 'Remaining to fund'],
     datasets: [
       {
         label: 'Amount (NGN)',
